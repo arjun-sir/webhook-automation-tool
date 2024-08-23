f240d650970aad8197d7b429df289d9d32055092
Arjun Khanna <arjunkhanna0108@gmail.com>
2024-08-24 02:13:13 +0530

    created transaction

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,14 +1,38 @@
 import express from "express"
+import {PrismaClient} from "@prisma/client"
+
+const client = new PrismaClient();
 
 const app = express();
-
+app.use(express.json())
 // https://hooks.zapier.com/hooks/catch/19895634/26g6l39/
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
     const userId = req.params.userId
     const zapId = req.params.zapId
+    const body = req.body
 
     // store in a db a new trigger
+    await client.$transaction(async tx =>{
+        const run = await client.zapRun.create({
+            data:{
+                zapId: zapId,
+                metadata: body
+            }
+        })
+
+        await client.zapRunOutbox.create({
+            data:{
+                zapRunId: run.id
+            }
+        })
+    })
 
     // push it onto a queue (kafka/redis)
+
+    res.json({
+        message: "Webhook recd"
+    })
 })
+
+app.listen(3000)
