832642d66521ddb8b4c725cdf266ba26d6795f18
Arjun Khanna <arjunkhanna0108@gmail.com>
2024-08-25 23:23:00 +0530

    added zap routes

--- primary-backend/src/router/zap.ts
+++ primary-backend/src/router/zap.ts
@@ -1,18 +1,110 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware";
+import { ZapCreateSchema } from "../types";
+import { prisma } from "../db";
 
 const router = Router();
 
-router.get("/", authMiddleware, (req, res) => {
-  res.send("signin handler");
+router.post("/", authMiddleware, async (req, res) => {
+    // @ts-ignore
+    const id = req.id;
+    const body = req.body;
+    const parsedData = ZapCreateSchema.safeParse(body);
+
+    if (!parsedData.success) {
+        return res.status(411).json({
+            message: "Invalid data",
+            data: parsedData.error.errors,
+        });
+    }
+
+    const zapId = await prisma.$transaction(async (tx) => {
+        const zap = await tx.zap.create({
+            data: {
+                userId: id,
+                triggerId: "",
+                Action: {
+                    create: parsedData.data.actions.map((action, index) => ({
+                        actionId: action.availableActionId,
+                        sortingOrder: index,
+                    })),
+                },
+            },
+        });
+
+        const trigger = await tx.trigger.create({
+            data: {
+                zapId: zap.id,
+                triggerId: parsedData.data.availableTriggerId,
+            },
+        });
+
+        await tx.zap.update({
+            where: {
+                id: zap.id,
+            },
+            data: {
+                triggerId: trigger.id,
+            },
+        });
+
+        return zap.id;
+    });
+
+    res.status(200).json({
+        message: "Zap created",
+        zapId: zapId,
+    });
 });
 
-router.post("/", authMiddleware, (req, res) => {
-  res.send("signin handler");
+router.get("/", authMiddleware, (req, res) => {
+    // @ts-ignore
+    const id = req.id;
+
+    const zaps = prisma.zap.findMany({
+        where: {
+            userId: id,
+        },
+        include: {
+            Action: {
+                select: {
+                    type: true,
+                },
+            },
+            trigger: {
+                select: {
+                    type: true,
+                },
+            },
+        },
+    });
+    res.status(200).json(zaps);
 });
 
 router.get("/:zapId", authMiddleware, (req, res) => {
-  res.send("zap handler");
+    // @ts-ignore
+    const id = req.id;
+    const zapId = req.params.zapId;
+
+    const zap = prisma.zap.findFirst({
+        where: {
+            userId: id,
+            id: zapId,
+        },
+        include: {
+            Action: {
+                select: {
+                    type: true,
+                },
+            },
+            trigger: {
+                select: {
+                    type: true,
+                },
+            },
+        },
+    });
+    res.status(200).json(zap);
 });
 
 export const zapRouter = router;
